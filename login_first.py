3f68f4d01ce5cac777c920527f2f307d1251c168
OneCodeMonkey <1460018362@qq.com>
2019-08-07 18:26:15 +0800

    fix

--- login_first.py
+++ login_first.py
@@ -3,4 +3,4 @@ from tasks import login
 
 if __name__ == '__main__':
     # you should execute this file, because celery timer will execute login delayed
-    login.excute_login_task()
+    login.execute_login_task()
