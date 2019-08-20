a71b4682085328dd2c8586b5ca022f89212487d3
OneCodeMonkey <1460018362@qq.com>
2019-08-20 13:34:01 +0000

    synchronize

--- login_first.py
+++
@@ -1,6 +0,0 @@
-# coding:utf-8
-from tasks import login
-
-if __name__ == '__main__':
-    # you should execute this file, because celery timer will execute login delayed
-    login.execute_login_task()
